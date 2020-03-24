04398115eb56d4c09751c33739a078219b719a55
haariypotter <52008110+haariypotter@users.noreply.github.com>
2020-03-24 11:33:40 +0530

    REFACTORED: Last name validation

--- src/main/java/com/userregistration/UserRegistration.java
+++ src/main/java/com/userregistration/UserRegistration.java
@@ -3,10 +3,16 @@ package com.userregistration;
 public class UserRegistration {
 
 
-    private static final String PATTERN_FIRSTNAME ="^[A-Z][a-z]{2,}$";
+    private static final String PATTERN_NAME ="^[A-Z][a-z]{2,}$";
 
 
     public static boolean checkFirstName(String firstName) {
-         return firstName.matches(PATTERN_FIRSTNAME);
+         return firstName.matches(PATTERN_NAME);
     }
+
+    public static boolean checkLastName(String lastName) {
+        return lastName.matches(PATTERN_NAME);
+    }
+
+
 }
--- src/test/java/com/userregistration/UserRegistrationTest.java
+++ src/test/java/com/userregistration/UserRegistrationTest.java
@@ -5,8 +5,25 @@ import org.junit.Test;
 
 public class UserRegistrationTest {
     @Test
-    public void whenGivenFirstName_withThreeOrMoreCharacter_and_firstCharacterCapital_shouldReturnTrue() {
+    public void givenFirstName_whenValid_thenTrue() {
         Assert.assertTrue(UserRegistration.checkFirstName("Gaurav"));
-
     }
+
+    @Test
+    public void givenFirstName_whenInValid_thenFalse() {
+        Assert.assertFalse(UserRegistration.checkFirstName("gaurav"));
+    }
+
+    @Test
+    public void givenLastName_whenValid_thenTrue() {
+        Assert.assertTrue(UserRegistration.checkLastName("Marathe"));
+    }
+
+    @Test
+    public void givenLastName_whenInValid_thenFalse() {
+        Assert.assertFalse(UserRegistration.checkLastName("marathe"));
+    }
+
+
+
 }
